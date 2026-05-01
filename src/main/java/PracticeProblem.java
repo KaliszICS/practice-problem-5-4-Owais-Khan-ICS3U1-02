f6c13703fdea61a56386f9c32a2be849febd9ab2
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-05-01 14:51:58 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,62 @@
+/**
+
+        * File: Lesson 5.4 - Swapping, reverse, split
+
+        * Author: Owais Ali Khan
+
+        * Date Created: May 1, 2026
+
+        * Date Last Modified: May 1, 2026
+
+        */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+	public static void stringSwap(String[] arr, int n1, int n2){
+        String temp;
+        temp = arr[n1];
+        arr[n1] = arr[n2];
+        arr[n2] = temp;
+
+    }    
+    
+    public static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        int temp;
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
+        
+    }
+    
+    public static String[] toWordArray(String words) {
+        String[] arr = words.split(" ");
+		int count = 0; 
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != "") {
+				count++;
+			}
+		}
+
+		String[] arr2 = new String[count];
+		int origIdx = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != "") {
+				arr2[origIdx] = arr[i];
+				origIdx++;
+			}
+		}
+
+		return arr2;
+    }
 
 }
